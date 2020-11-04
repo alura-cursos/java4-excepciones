54c65509995fcacde7986832e86011058855407a
Diego Rojas <darguelles.rojas91@gmail.com>
2020-11-04 18:13:06 -0300

    Try com recursos

--- src/TestConexion.java
+++ src/TestConexion.java
@@ -3,21 +3,28 @@ public class TestConexion {
 	
 	public static void main(String[] args) throws Exception {
 		
-		
-		Conexion con = null;
-		try {
-			con = new Conexion();
+		try (Conexion con = new Conexion()) {
 			con.leerDatos();
-		} 
-		catch (IllegalStateException ex) {
+		} catch (IllegalStateException ex) {
+			System.out.println("Ejecutando catch");
 			ex.printStackTrace();
 		}
-		finally {
-			System.out.println("Ejecutando finally");
-			if (con != null) {
-				con.cerrar();
-			}
-		} 
+		
+		
+//		Conexion con = null;
+//		try {
+//			con = new Conexion();
+//			con.leerDatos();
+//		} 
+//		catch (IllegalStateException ex) {
+//			ex.printStackTrace();
+//		}
+//		finally {
+//			System.out.println("Ejecutando finally");
+//			if (con != null) {
+//				con.cerrar();
+//			}
+//		} 
 
 	}
 	
