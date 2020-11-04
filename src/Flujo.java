cc3e2ec3f07fce4c493f47002b0525d10c7ac2c5
Diego Rojas <darguelles.rojas91@gmail.com>
2020-11-04 18:01:54 -0300

    Lanzando excepcion

--- src/Flujo.java
+++ src/Flujo.java
@@ -11,22 +11,18 @@ public class Flujo {
         System.out.println("Inicio de metodo1");
         try {
             metodo2();
-        } catch(ArithmeticException ex) {
-            System.out.println("ArithmeticException");
+        } catch(ArithmeticException | NullPointerException ex) {
+            String msg = ex.getMessage();
+            System.out.println("Exception " + msg);
+            ex.printStackTrace();
         }
         System.out.println("Fin de metodo1");
     }
 
     private static void metodo2() {
         System.out.println("Inicio de metodo2");
-        for(int i = 1; i <= 5; i++) {
-            System.out.println(i);
-            int a = i / 0;
-        }
-        Cuenta c = null;
-        c.deposita();
-        
-        System.out.println("Fin de metodo2");
+        throw new ArithmeticException("dio error");
+        // System.out.println("Fin de metodo2");
     }
 
 	
