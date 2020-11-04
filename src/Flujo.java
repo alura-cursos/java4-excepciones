0cdc47c029d99697f7466ce953cc78c807a43b8d
Diego Rojas <darguelles.rojas91@gmail.com>
2020-11-04 17:50:36 -0300

    Clase 1

---
+++ src/Flujo.java
@@ -0,0 +1,25 @@
+
+public class Flujo {
+
+	public static void main(String[] args) {
+        System.out.println("Inicio main");
+        metodo1();
+        System.out.println("Fin de main");
+    }
+
+    private static void metodo1() {
+        System.out.println("Inicio de metodo1");
+        metodo2();
+        System.out.println("Fin de metodo1");
+    }
+
+    private static void metodo2() {
+        System.out.println("Inicio de metodo2");
+        for(int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Fin de metodo2");
+    }
+
+	
+}
